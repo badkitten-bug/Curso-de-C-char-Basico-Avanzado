0ffd752be8e1a03edbbafb19a630049e8661d8eb
Bug <seghsteve1997@gmail.com>
2022-12-20 23:04:12 -0500

    IntroduccionAc#

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information.
+//Ejercicio 1
+//escribir un programa que pida el nombre y lo muestre por pantalla
+
+Console.WriteLine("Introduce tu nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola " + nombre + "!");
+
+//Ejercicio 2
+//escribir un programa que tome la hora y la escriba por pantalla
+
+Console.WriteLine("Introduce la hora: ");
+string hora = Console.ReadLine();
+Console.WriteLine("La hora es: " + hora);
